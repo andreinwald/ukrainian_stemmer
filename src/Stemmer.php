cc08ea496f8109f248f9f27803f81b82beb2faa7
a <a@a>
2023-02-22 10:58:11 +0100

    moving step1 to separate method for easier reading

--- src/Stemmer.php
+++ src/Stemmer.php
@@ -10,13 +10,13 @@ namespace UkrainianStemmer;
 class Stemmer
 {
     const INFINITIVE = '/(ти|учи|ячи|вши|ши|ати|яти|ючи)$/u';
-    const PERFECTIVEGROUND = '/(ив|ивши|ившись((?<=[ая])(в|вши|вшись)))$/u';
+    const PERFECTIVE_GROUND = '/(ив|ивши|ившись((?<=[ая])(в|вши|вшись)))$/u';
     const REFLEXIVE = '/(с[яьи])$/u';
     const ADJECTIVE = '/(ими|ій|ий|а|е|ова|ове|ів|є|їй|єє|еє|я|ім|ем|им|их|іх|ою|йми|іми|у|ю|ого|ому|ої)$/u';
     const PARTICIPLE = '/(ий|ого|ому|им|ім|а|ій|у|ою|і|их|йми)$/u';
-    const VERB = '/(сь|ся|ив|ать|ять|у|ю|ав|али|учи|ячи|вши|ши|е|ме|ати|яти|є)$/u';
-    const NOUN = '/(а|ев|ов|е|ями|ами|еи|и|ей|ой|ий|й|иям|ям|ием|ам|ом|о|у|ах|иях|ях|ь|ию|ью|ю|ия|ья|я|і|ові|ї|ею|єю|ою|є|еві|ем|єм|ів|їв|\'ю)$/u';
-    const FIRSTVOWEL = '/^(.*?[аеиоуюяіїє])(.*)$/u';
+    const VERB_ENDING = '/(сь|ся|ив|ать|ять|у|ю|ав|али|учи|ячи|вши|ши|е|ме|ати|яти|є)$/u';
+    const NOUN_ENDING = '/(а|ев|ов|е|ями|ами|еи|и|ей|ой|ий|й|иям|ям|ием|ам|ом|о|у|ах|иях|ях|ь|ию|ью|ю|ия|ья|я|і|ові|ї|ею|єю|ою|є|еві|ем|єм|ів|їв|\'ю)$/u';
+    const FIRST_VOWEL = '/^(.*?[аеиоуюяіїє])(.*)$/u';
     const DERIVATIONAL = '/[^аеиоуюяіїє][аеиоуюяіїє]+[^аеиоуюяіїє]+[аеиоуюяіїє].*сть?$/u';
 
     public static function stemWord(string $word): string
@@ -27,37 +27,18 @@ class Stemmer
             return $word;
         }
 
-        preg_match(self::FIRSTVOWEL, $stem, $matches);
+        preg_match(self::FIRST_VOWEL, $stem, $matches);
         if (!$matches) {
             return $word;
         }
         // RV is the region after the first vowel, or the end of the word if it contains no vowel.
-        $RV = $matches[2][0];
-        $baseWithFirstVowel = $matches[1][0];
-
-        /*
-         * Step 1: Search for a PERFECTIVE GERUND ending. If one is found remove it, and that is then the end of step 1.
-         * Otherwise try and remove a REFLEXIVE ending, and then search in turn for (1) an ADJECTIVAL, (2) a VERB or (3) a NOUN ending.
-         * As soon as one of the endings (1) to (3) is found remove it, and terminate step 1.
-         */
-        $m = preg_replace(self::PERFECTIVEGROUND, '', $RV);
-        if (strcmp($m, $RV) === 0) {
-            $RV = preg_replace(self::REFLEXIVE, '', $RV);
-            $m = preg_replace(self::ADJECTIVE, '', $RV);
-            if (strcmp($m, $RV) === 0) {
-                $RV = preg_replace(self::PARTICIPLE, '', $RV);
-            } else {
-                $RV = $m;
-                $m = preg_replace(self::VERB, '', $RV);
-                if (strcmp($m, $RV) === 0) {
-                    $RV = preg_replace(self::NOUN, '', $RV);
-                } else {
-                    $RV = $m;
-                }
-            }
-        } else {
-            $RV = $m;
+        $RV = $matches[2];
+        $baseWithFirstVowel = $matches[1];
+        if (!mb_strlen($baseWithFirstVowel) || !mb_strlen($RV)) {
+            return $word;
         }
+
+        $RV = self::step1($RV);
 
         /*
          * Step 2: If the word ends with i, remove it.
@@ -83,8 +64,31 @@ class Stemmer
             $RV = $m;
         }
 
-        $stem = $baseWithFirstVowel . $RV;
+        return $baseWithFirstVowel . $RV;
+    }
 
-        return $stem;
+    private static function step1($RV)
+    {
+        // Step 1: Search for a PERFECTIVE GERUND ending. If one is found remove it, and that is then the end of step 1.
+        $replacedGro = preg_replace(self::PERFECTIVE_GROUND, '', $RV);
+        if ($replacedGro && $replacedGro != $RV) {
+            return $replacedGro;
+        }
+        // Otherwise, try and remove a REFLEXIVE ending, and then search in turn for (1) an ADJECTIVAL, (2) a VERB or (3) a NOUN ending.
+        // As soon as one of the endings (1) to (3) is found remove it, and terminate step 1.
+        $RV = preg_replace(self::REFLEXIVE, '', $RV);
+        $replacedAdj = preg_replace(self::ADJECTIVE, '', $RV);
+        if ($replacedAdj && $replacedAdj != $RV) {
+            return preg_replace(self::PARTICIPLE, '', $replacedAdj);
+        }
+        $replacedVerb = preg_replace(self::VERB_ENDING, '', $RV);
+        if ($replacedVerb && $replacedVerb != $RV) {
+            return $replacedVerb;
+        }
+        $replacedNoun = preg_replace(self::NOUN_ENDING, '', $RV);
+        if ($replacedNoun && $replacedNoun != $RV) {
+            return $replacedNoun;
+        }
+        return $RV;
     }
 }
